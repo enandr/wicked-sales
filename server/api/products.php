a0f9ee15b5a9fe95de62b5e397d86507579277b8
Roger Enand <rogerenand@gmail.com>
2019-11-19 19:10:12 +0000

    put into 2 functions

--- server/api/products.php
+++ server/api/products.php
@@ -3,31 +3,37 @@
 if ($request['method'] === 'GET' || $request['method'] === 'get') {
   $id = $_GET['productId'];
   $link = get_db_link();
-  $products = get_all_products($link,$id);
+  if(!empty($id)){
+    $products = get_single_product($link, $id);
+  }
+  else{
+    $products = get_all_products($link);
+  }
   $response['body'] = $products;
   send($response);
 }
-function get_all_products($link,$id)
-{
-  if ($_GET['productId'] >= 1) {
+function get_single_product($link, $id){
+  $product = $_GET['productId'];
+  if ($product >= 1) {
     $sql = "SELECT * FROM `products` WHERE `productId`=$id";
     $res = mysqli_query($link, $sql);
     $output = mysqli_fetch_assoc($res);
-  }
-  else if ($_GET['productId'] <= 0 && !empty($id)){
+  } else if ($product <= 0) {
     throw new ApiError("ID '$id' is not valid.", 400);
   }
-  else{
+  if (count($output) === 0) {
+    throw new ApiError("Product with ID '$id' does not exist.", 404);
+  }
+  return $output;
+}
+function get_all_products($link){
     $sql = "SELECT `productId`, `name`, `price`, `image`, `shortDescription` FROM `products`";
     $res = mysqli_query($link, $sql);
     $output = [];
     while ($row = mysqli_fetch_assoc($res)) {
       array_push($output, $row);
     }
-  }
-  if (count($output)===0){
-    throw new ApiError("Product with ID '$id' does not exist.", 404);
-  }
+
   return $output;
 }
 send($response);
