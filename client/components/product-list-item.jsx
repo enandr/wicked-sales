76d787acd401602a3658a05d8deefc729c8bfb57
Roger Enand <rogerenand@gmail.com>
2019-11-19 01:54:39 +0000

    fixed price

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 class ProductListItem extends React.Component {
   render() {
+    const newPrice = (this.props.product.price / 100).toFixed(2);
     return (
       <div className="card products" >
         <img src={this.props.product.image} className=""></img>
         <div className="card-body">
           <h5 className="card-title">{this.props.product.name}</h5>
-          <p className="card-text">${this.props.product.price}</p>
+          <p className="card-text">${newPrice}</p>
           <p className="card-text short-description">{this.props.product.shortDescription}</p>
         </div>
       </div>
