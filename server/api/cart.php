4e50fb24d5e54de41d53406af4c37a3e4c9d4fe8
Roger Enand <rogerenand@gmail.com>
2019-11-20 20:31:14 +0000

    finsihed back end add to cart

--- server/api/cart.php
+++ server/api/cart.php
@@ -1,37 +1,69 @@
 <?php
-if ($request['method'] === 'GET') {
-  if (!isset($_SESSION['cart_id'])){
-    $response['body'] =[];
+$link = get_db_link();
+
+
+  if ($request['method'] === 'GET') {
+    $cartId  = getCartId($link);
+    $response['body'] = getCartItems($link, $cartId);
     send($response);
   }
-  else{
-    $cartId  = $_SESSION['cart_id'];
+
+  if ($request['method'] === 'POST') {
+    $product = $request['body']['productId'];
+    $cartId = getCartId($link);
+    $addedItem = addToCart($link, $cartId, $product);
+    $response['body'] = $addedItem;
+    send($response);
+  }
+  // !---------------------------
+  function getPrice($link, $product){
+    if (!isset($product)) {
+      throw new ApiError("'$product' is not a valid ID", 400);
+    }
+    $sqlPrice = "SELECT `price` FROM `products` WHERE `productId` = $product";
+    $resPrice = mysqli_query($link, $sqlPrice);
+    $resPrice =  mysqli_fetch_assoc($resPrice);
+    if (!isset($resPrice)) {
+      throw new ApiError("ID '$product' could not be found", 404);
+    }
+    return $resPrice;
+  }
+
+  function newCart($link){
+    $sqlTime = "INSERT INTO `carts` (`createdAt`) VALUES (CURRENT_TIMESTAMP)";
+    mysqli_query($link, $sqlTime);
+    $cartId = $link->insert_id;
+    $_SESSION['cart_id'] = $cartId;
+    return $cartId;
+  }
+
+  function addToCart($link, $cartId, $product){
+    $price = getPrice($link, $product);
+    $price = $price['price'];
+    $sqlCartInsert = "INSERT INTO `cartItems` (`cartId`, `productId`, `price`) VALUES ($cartId, $product, $price)";
+    mysqli_query($link, $sqlCartInsert);
+    $insertId = $link -> insert_id;
+    $resCartJoinSql = "SELECT cartItems.cartItemId, products.productId,products.name,products.price,products.image,products.shortDescription FROM cartItems INNER JOIN products ON cartItems.productId=products.productId WHERE `cartItemId` = $insertId";
+    $addedItemRes = mysqli_query($link, $resCartJoinSql);
+    $response = mysqli_fetch_assoc($addedItemRes);
+    return $response;
+  }
+
+  function getCartId($link){
+    if (!isset($_SESSION['cart_id'])) {
+      $cartId = newCart($link);
+    } else {
+      $cartId  = $_SESSION['cart_id'];
+    }
+    return $cartId;
+  }
+
+  function getCartItems($link,$cartId){
     $cartGetSql = "SELECT * FROM `cartItems` WHERE `cartId` = $cartId";
     $cartGetRes = mysqli_query($link, $cartGetSql);
     $output = [];
-    while ($row = mysqli_fetch_assoc($res)) {
+    while ($row = mysqli_fetch_assoc($cartGetRes)) {
       array_push($output, $row);
     }
-    $response['body'] = $output;
-    send($response);
-  }
-}
-  if ($request['method'] === 'POST') {
-    $product = $request['body']['productId'];
-    if (!isset($product)) {
-      throw new ApiError("'$product' is not a valid ID",400);
-    }
-    $link = get_db_link();
-    $sqlPrice = "SELECT `price` FROM `products` WHERE `productId` = $product";
-    $sqlTime = "INSERT INTO `carts` (`createdAt`) VALUES (CURRENT_TIMESTAMP)";
-    $resPrice = mysqli_query($link, $sqlPrice);
-    $resTime = mysqli_query($link, $sqlTime);
-    $timeInsertId = mysqli_insert_id($link);
-    $sqlCartInsert = "INSERT INTO `cartItems` (`cartId`, `productId`, `price`) VALUES ($timeInsertId, $product, $price)";
-    $resCartInsert = mysqli_query($link, $sqlCartInsert);
-    $cartInsertId = mysqli_insert_id($link);
-    $resCartJoin = "SELECT cartItems.cartItemId, products.productId,products.name,products.price,products.image,products.shortDescription FROM cartItems INNER JOIN products ON cartItems.productId=products.productId";
-    $response['body'] = mysqli_query($link, $resCartJoin);
-    $_SESSION['cart_id'] = $timeInsertId;
-    send($response);
+    return $output;
   }
